3fe6f2dacade95d1b5ac4df3b411d8799543f78b
Luiz M. Faria <maltezfaria@gmail.com>
2023-05-24 09:42:39 +0200

    finish formatting files

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,22 @@
 using PolynomialSolutions
 using Documenter
 
-DocMeta.setdocmeta!(PolynomialSolutions, :DocTestSetup, :(using PolynomialSolutions); recursive=true)
+DocMeta.setdocmeta!(PolynomialSolutions, :DocTestSetup, :(using PolynomialSolutions);
+                    recursive=true)
 
 makedocs(;
-    modules=[PolynomialSolutions],
-    authors="Luiz M. Faria",
-    repo="https://github.com/WaveProp/PolynomialSolutions.jl/blob/{commit}{path}#{line}",
-    sitename="PolynomialSolutions.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://WaveProp.github.io/PolynomialSolutions.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Docstrings" => "docstrings.md"
-    ],
-)
+         modules=[PolynomialSolutions],
+         authors="Luiz M. Faria",
+         repo="https://github.com/WaveProp/PolynomialSolutions.jl/blob/{commit}{path}#{line}",
+         sitename="PolynomialSolutions.jl",
+         format=Documenter.HTML(;
+                                prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://WaveProp.github.io/PolynomialSolutions.jl",
+                                edit_link="main",
+                                assets=String[]),
+         pages=["Home" => "index.md",
+                "Docstrings" => "docstrings.md"])
 
 deploydocs(;
-    repo="github.com/WaveProp/PolynomialSolutions.jl",
-    devbranch="main",
-)
+           repo="github.com/WaveProp/PolynomialSolutions.jl",
+           devbranch="main")
